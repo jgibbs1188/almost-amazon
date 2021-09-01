aec7c910f878cd54a65e6743550ea51245c51480
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-08-31 20:54:58 -0500

    MVP done. Viewing authors and all of their books. Deleting an author delets all of their books..

--- src/javascripts/components/viewAuthor.js
+++ src/javascripts/components/viewAuthor.js
@@ -13,8 +13,22 @@ const viewAuthor = (obj) => {
         <div class="mt-5">
           <i id="edit-author-btn--${obj.firebaseKey}" class="fas fa-edit btn btn-info"></i>
           <i id="delete-author--${obj.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+          <hr>
+          <h3 class="author-book-title">Books by this Author</h3>
         </div>
       </div>`;
+
+  obj.book.forEach((item) => {
+    document.querySelector('#author-books').innerHTML += `
+          <div class="author-card">
+            <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
+            <div class="card-body" style="height: 100px;">
+              <h5  class="text-white ms-5 details card-title">${item.title}</h5>
+              <p  class="text-white ms-5 details card-text bold">${item.sale ? `<span class="badge bg-info"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
+            </div>
+          </div>
+        `;
+  });
 };
 
 export default viewAuthor;
